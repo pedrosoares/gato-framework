d7313c2293af554c69edf657414d90ca0b5a812e
Pedro Soares <contato@pedrosoares.net>
2020-10-02 12:47:33 -0300

    Updated to the last core version

--- src/app/index_controller.rs
+++ src/app/index_controller.rs
@@ -1,17 +1,16 @@
-use serde_json::json;
 use gato_core::kernel::{Request, Response, Logger};
 
 pub struct IndexController {}
 impl IndexController {
     pub fn index(request: &Request) -> Response {
         Logger::info("IndexController[index]");
-        let params = request.all();
-        return Response::json(json!(params));
+        let params = request.json();
+        return Response::json(params);
     }
 
     pub fn test(request: &Request) -> Response {
         Logger::info("IndexController[test]");
-        let id = request.get("id", "");
+        let id = request.get_param("id", "");
         return Response::new(id.as_str());
     }
 }
